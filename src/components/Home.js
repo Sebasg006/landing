15ef6fbc4888f4532887a10c12887995f1057f0f
Sebas_g006 <sebasgiraldoanaya@gmail.com>
2025-12-21 22:43:40 -0500

    new changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ import React from "react";
 import Navbar from "./Navbar";
 const Home = (props) => {
     return(<div className="Home-container">
-        <Navbar/>
         <h1>{props.title}</h1>
+        <h1>Testeo</h1>
         </div>
     )
 }
